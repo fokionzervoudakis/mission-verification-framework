6b9f7d10da995400d0ff4f430d2d8ea6630e0b02
zerdaks <7451424+zerdaks@users.noreply.github.com>
2024-10-18 01:23:18 +0100

    feat: improve classifier output

--- prolog-kb/observer_rules.pl
+++ prolog-kb/observer_rules.pl
@@ -2,6 +2,14 @@
 observer(Y) :-
     crosscutting_precondition(_, _, _, Y).
 
+% Check with better output
+check_default_observer(X) :-
+    (
+        default_observer(X)
+    ->  format('\e[32mSuccess\e[0m: \e[34m~w\e[0m is a \e[33mdefault observer\e[0m~n', [X])
+    ;   format('\e[31mWarning\e[0m: \e[34m~w\e[0m is not a \e[33mdefault observer\e[0m~n', [X])
+    ).
+
 default_observer(X) :-
     observer(X),
     not(observer_precondition(X)).
@@ -15,13 +23,37 @@ constrained_observer(Y) :-
 sibling_observer(Y) :-
     sibling_precondition(_, _, _, Y).
 
+% Check with better output
+check_terminal_observer(X) :-
+    (
+        terminal_observer(X)
+    ->  format('\e[32mSuccess\e[0m: \e[34m~w\e[0m is a \e[33mterminal observer\e[0m~n', [X])
+    ;   format('\e[31mWarning\e[0m: \e[34m~w\e[0m is not a \e[33mterminal observer\e[0m~n', [X])
+    ).
+
 terminal_observer(X) :-
     observer(X),
     terminal(X).
 
+% Check with better output
+check_terminal_constrained_observer(X) :-
+    (
+        terminal_constrained_observer(X)
+    ->  format('\e[32mSuccess\e[0m: \e[34m~w\e[0m is a \e[33mterminal constrained observer\e[0m~n', [X])
+    ;   format('\e[31mWarning\e[0m: \e[34m~w\e[0m is not a \e[33mterminal constrained observer\e[0m~n', [X])
+    ).
+
 terminal_constrained_observer(X) :-
     constrained_observer(X),
     terminal(X).
+
+% Check with better output
+check_observer_precondition(X) :-
+    (
+        observer_precondition(X)
+    ->  format('\e[32mSuccess\e[0m: \e[34m~w\e[0m is an \e[33mobserver precondition\e[0m~n', [X])
+    ;   format('\e[31mWarning\e[0m: \e[34m~w\e[0m is not an \e[33mobserver precondition\e[0m~n', [X])
+    ).
 
 /* SWRL rule */
 observer_precondition(X) :-
